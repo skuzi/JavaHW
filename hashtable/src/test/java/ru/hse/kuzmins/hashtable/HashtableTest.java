f86f9260857bac60e69a182b0de0fc3879549457
Serg Kuzmin <serg.kuzmin00@mail.ru>
2019-02-11 16:31:46 +0300

    fix abbreviations in HashTableTest

--- hashtable/src/test/java/ru/hse/kuzmins/hashtable/HashtableTest.java
+++ hashtable/src/test/java/ru/hse/kuzmins/hashtable/HashtableTest.java
@@ -7,209 +7,209 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class HashTableTest {
 
-    private HashTable ht;
+    private HashTable table;
 
     @BeforeEach
     void initTable() {
-        ht = new HashTable();
+        table = new HashTable();
     }
 
 
     @Test
     void sizeEmpty() {
-        assertEquals(0, ht.size());
+        assertEquals(0, table.size());
     }
 
     @Test
     void sizeAddSameKeys() {
-        ht.put("a", "a");
-        assertEquals(1, ht.size());
-        ht.put("a", "b");
-        assertEquals(1, ht.size());
+        table.put("a", "a");
+        assertEquals(1, table.size());
+        table.put("a", "b");
+        assertEquals(1, table.size());
     }
 
     @Test
     void sizeAddFew() {
         for (int i = 0; i < 10; i++) {
-            ht.put("a" + i, "a");
-            assertEquals(i + 1, ht.size());
+            table.put("a" + i, "a");
+            assertEquals(i + 1, table.size());
         }
     }
 
     @Test
     void sizeAfterRemoval() {
         for (int i = 0; i < 10; i++) {
-            ht.put(String.valueOf(i), String.valueOf(i + 1));
+            table.put(String.valueOf(i), String.valueOf(i + 1));
         }
 
         for (int i = 0; i < 5; i++) {
-            assertEquals(10 - i, ht.size());
-            ht.remove(String.valueOf(2 * i));
+            assertEquals(10 - i, table.size());
+            table.remove(String.valueOf(2 * i));
         }
-        assertEquals(5, ht.size());
+        assertEquals(5, table.size());
     }
 
     @Test
     void sizeAfterClear() {
         for (int i = 0; i < 200; i++) {
-            ht.put(String.valueOf(i), String.valueOf(i + 1));
+            table.put(String.valueOf(i), String.valueOf(i + 1));
         }
 
-        ht.clear();
-        assertEquals(0, ht.size());
+        table.clear();
+        assertEquals(0, table.size());
     }
 
     @Test
     void containsEmpty() {
-        assertFalse(ht.contains("abc"));
-        assertFalse(ht.contains(""));
-        assertFalse(ht.contains("x"));
+        assertFalse(table.contains("abc"));
+        assertFalse(table.contains(""));
+        assertFalse(table.contains("x"));
     }
 
     @Test
     void containsAfterAdd() {
-        ht.put("x", "abc");
-        assertFalse(ht.contains("abc"));
-        assertTrue(ht.contains("x"));
+        table.put("x", "abc");
+        assertFalse(table.contains("abc"));
+        assertTrue(table.contains("x"));
     }
 
     @Test
     void containsAfterPuttingSameKeys() {
-        ht.put("x", "cde");
-        assertTrue(ht.contains("x"));
-        assertFalse(ht.contains("cde"));
+        table.put("x", "cde");
+        assertTrue(table.contains("x"));
+        assertFalse(table.contains("cde"));
 
-        ht.put("x", "abc");
-        assertFalse(ht.contains("abc"));
-        assertTrue(ht.contains("x"));
+        table.put("x", "abc");
+        assertFalse(table.contains("abc"));
+        assertTrue(table.contains("x"));
     }
 
     @Test
     void containsAfterRemoval() {
-        ht.put("abc", "abc");
-        ht.put("x", "abc");
-        assertTrue(ht.contains("abc"));
+        table.put("abc", "abc");
+        table.put("x", "abc");
+        assertTrue(table.contains("abc"));
 
-        ht.remove("x");
-        assertFalse(ht.contains("x"));
-        assertTrue(ht.contains("abc"));
+        table.remove("x");
+        assertFalse(table.contains("x"));
+        assertTrue(table.contains("abc"));
     }
 
     @Test
     void containsAfterClear() {
-        ht.put("abc", "abc");
-        ht.put("x", "y");
-        ht.clear();
+        table.put("abc", "abc");
+        table.put("x", "y");
+        table.clear();
 
-        assertFalse(ht.contains("abc"));
+        assertFalse(table.contains("abc"));
 
-        ht.put("x", "y");
-        assertTrue(ht.contains("x"));
+        table.put("x", "y");
+        assertTrue(table.contains("x"));
     }
 
     @Test
     void getFromEmptyTable() {
-        assertNull(ht.get("abc"));
+        assertNull(table.get("abc"));
     }
 
     @Test
     void getExistingKey() {
-        ht.put("abc", "xyz");
-        assertEquals("xyz", ht.get("abc"));
-        ht.put("xyz", "abc");
-        assertEquals("abc", ht.get("xyz"));
+        table.put("abc", "xyz");
+        assertEquals("xyz", table.get("abc"));
+        table.put("xyz", "abc");
+        assertEquals("abc", table.get("xyz"));
     }
 
     @Test
     void getAfterRemoval() {
-        ht.put("abc"    , "abc");
-        assertEquals("abc", ht.get("abc"));
+        table.put("abc"    , "abc");
+        assertEquals("abc", table.get("abc"));
 
-        ht.remove("abc");
-        assertNull(ht.get("abc"));
+        table.remove("abc");
+        assertNull(table.get("abc"));
     }
 
     @Test
     void getAfterClear() {
         for (int i = 0; i < 10; i++) {
-            ht.put(String.valueOf(i), "123");
+            table.put(String.valueOf(i), "123");
         }
 
-        ht.clear();
+        table.clear();
         for (int i = 0; i < 10; i++) {
-            assertNull(ht.get(String.valueOf(i)));
+            assertNull(table.get(String.valueOf(i)));
         }
     }
 
     @Test
     void getAfterSeveralPutsToSamePosition() {
         for (int i = 0; i < 10; i++) {
-            ht.put("1", String.valueOf(i));
-            assertEquals(String.valueOf(i), ht.get("1"));
+            table.put("1", String.valueOf(i));
+            assertEquals(String.valueOf(i), table.get("1"));
         }
     }
 
     @Test
     void putToEmptyPosition() {
-        String val = ht.put("abc", "xyz");
-        assertNull(val);
+        String value = table.put("abc", "xyz");
+        assertNull(value);
     }
 
     @Test
     void putToOccupiedPosition() {
-        String val = ht.put("abc", "xyz");
+        String value = table.put("abc", "xyz");
 
-        val = ht.put("abc", "abc");
-        assertEquals("xyz", val);
+        value = table.put("abc", "abc");
+        assertEquals("xyz", value);
     }
 
     @Test
     void putAfterRemoval() {
-        ht.put("abc", "xyz");
+        table.put("abc", "xyz");
 
-        ht.remove("abc");
-        String val = ht.put("abc", "a");
-        assertNull(val);
+        table.remove("abc");
+        String value = table.put("abc", "a");
+        assertNull(value);
     }
 
     @Test
     void removeKeysWithCollision() {
-        ht.put("a", "xyz");
-        ht.put("k", "abc");
+        table.put("a", "xyz");
+        table.put("k", "abc");
 
-        ht.remove("k");
-        assertTrue(ht.contains("a"));
-        assertFalse(ht.contains("u"));
-        assertFalse(ht.contains("k"));
+        table.remove("k");
+        assertTrue(table.contains("a"));
+        assertFalse(table.contains("u"));
+        assertFalse(table.contains("k"));
     }
 
     @Test
     void removeAfterSeveralPuts() {
         for (int i = 0; i < 20; i++) {
-            ht.put("a" + i, "b" + i);
+            table.put("a" + i, "b" + i);
         }
 
         for (int i = 0; i < 20; i++) {
-            assertTrue(ht.contains("a" + i));
+            assertTrue(table.contains("a" + i));
         }
 
         for (int i = 20; i < 30; i++) {
-            assertFalse(ht.contains("a" + i));
+            assertFalse(table.contains("a" + i));
         }
     }
 
     @Test
     void clearEmpty() {
-        ht.clear();
-        assertEquals(0, ht.size());
+        table.clear();
+        assertEquals(0, table.size());
     }
 
     @Test
     void clearAfterManyPuts() {
         for (int i = 0; i < 50000; i++) {
-            ht.put(String.valueOf(i), String.valueOf(i + 10));
+            table.put(String.valueOf(i), String.valueOf(i + 10));
         }
-        ht.clear();
-        assertEquals(0, ht.size());
+        table.clear();
+        assertEquals(0, table.size());
     }
 }
