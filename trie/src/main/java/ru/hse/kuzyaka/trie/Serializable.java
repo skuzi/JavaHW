60206a0ed69b3db3b1c15b3e717bff3727bb73db
skuzi <32015627+skuzi@users.noreply.github.com>
2019-02-14 15:05:19 +0300

    Add javadoc

--- trie/src/main/java/ru/hse/kuzyaka/trie/Serializable.java
+++ trie/src/main/java/ru/hse/kuzyaka/trie/Serializable.java
@@ -4,9 +4,15 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
+/**
+ * Interface provides to work with specific data (send it or get it) in a similiar way as with bytestreams.
+ * More specifically, it has two methods -- {@code deserialize(InputStream)} and {@code serialize(OutputStream)} which are used
+ * to send and receive data. Any class implementing this interface must provide an implementation for both methods.
+ * Note that if working with streams goes wrong, an {@code IOException} is thrown.
+ */
 public interface Serializable {
     /**
-     * Writes this trie as a byte sequence
+     * Writes this object as a byte sequence
      *
      * @param out <code>OutputStream</code> to write trie to
      * @throws IOException
@@ -14,7 +20,7 @@ public interface Serializable {
     void serialize(OutputStream out) throws IOException;
 
     /**
-     * Reads a trie from the given <code>InputStream</code>. Any data which the trie contained earlier, is discarded.
+     * Reads an object from the given <code>InputStream</code>. Any data which the object contained earlier, is discarded.
      *
      * @param in <code>InputStream</code> to read from
      * @throws IOException
