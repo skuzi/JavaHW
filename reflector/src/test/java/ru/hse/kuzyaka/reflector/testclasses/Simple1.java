3e4f25948c9d93b793688336e46002a566ab033d
Serg Kuzmin <serg.kuzmin00@mail.ru>
2019-03-14 02:06:42 +0300

    add missing test classes

---
+++ reflector/src/test/java/ru/hse/kuzyaka/reflector/testclasses/Simple1.java
@@ -0,0 +1,12 @@
+package ru.hse.kuzyaka.reflector.testclasses;
+
+public class Simple1 {
+    public int y;
+    int x;
+
+    void lol() {
+    }
+
+    void kek() {
+    }
+}
---
+++ reflector/src/test/java/ru/hse/kuzyaka/reflector/testclasses/Simple2.java
@@ -0,0 +1,12 @@
+package ru.hse.kuzyaka.reflector.testclasses;
+
+public class Simple2 {
+    int x;
+    int y;
+
+    void lol() {
+    }
+
+    private void kek() {
+    }
+}
