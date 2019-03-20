63d8409fbea48917f2d4c8c5b2108546d7c88d5e
Serg Kuzmin <serg.kuzmin00@mail.ru>
2019-03-20 14:53:33 +0300

    add draw situation to time comparison, minor fixes

--- qsort/src/main/java/ru/hse/kuzyaka/qsort/SortTimeComparator.java
+++ qsort/src/main/java/ru/hse/kuzyaka/qsort/SortTimeComparator.java
@@ -6,24 +6,29 @@ import java.util.stream.IntStream;
 
 public class SortTimeComparator {
     public static void main(String[] args) {
-        for(int i = 2; i < (1 << 22); i *= 2) {
+        QuickSort.setSortThreshold(30);
+        for(int i = 16; i < (1 << 22); i *= 2) {
             var list = new ArrayList<Integer>();
             IntStream.range(0, i).forEach(list::add);
             Collections.shuffle(list);
 
-            double timeCollectionsSort = getTime(() -> Collections.sort(list));
-            double timeQuickSort = getTime(() -> QuickSort.sort(list));
-            String winner = timeQuickSort > timeCollectionsSort ? "Collections::sort" : "QuickSort::sort";
-            System.out.println(
-                    "On " + i + " integers " + winner + " wins by " +
-                            Math.abs(timeCollectionsSort - timeQuickSort) + "milliseconds");
+            long timeCollectionsSort = getTime(() -> Collections.sort(list));
+            long timeQuickSort = getTime(() -> QuickSort.sort(list));
+            System.out.print("On " + i + " integers ");
+            if (timeQuickSort == timeCollectionsSort) {
+                System.out.println("draw occurred");
+            } else {
+                String winner = timeQuickSort > timeCollectionsSort ? "Collections::sort" : "QuickSort::sort";
+                System.out.println(winner + " wins by " +
+                                Math.abs(timeCollectionsSort - timeQuickSort) + "milliseconds");
+            }
         }
     }
 
-    private static double getTime(Runnable function) {
-        double timeBegin = System.currentTimeMillis();
+    private static long getTime(Runnable function) {
+        long timeBegin = System.currentTimeMillis();
         function.run();
-        double timeEnd = System.currentTimeMillis();
+        long timeEnd = System.currentTimeMillis();
         return timeEnd - timeBegin;
     }
 }
