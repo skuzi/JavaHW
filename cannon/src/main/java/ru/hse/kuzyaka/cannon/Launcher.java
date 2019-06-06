9338d1cac6c8dccdaa281287d70c545b73764187
Serg Kuzmin <serg.kuzmin00@mail.ru>
2019-06-06 23:56:48 +0300

    add doc to launcher

--- cannon/src/main/java/ru/hse/kuzyaka/cannon/Launcher.java
+++ cannon/src/main/java/ru/hse/kuzyaka/cannon/Launcher.java
@@ -1,6 +1,12 @@
 package ru.hse.kuzyaka.cannon;
 
+/** Launcher for the application **/
 public class Launcher {
+    /**
+     * Launches the application with the given command line arguments
+     *
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
         Main.run(args);
     }
